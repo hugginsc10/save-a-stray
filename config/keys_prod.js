25dd3d347d6d79855ca010e1302f005ee6efaeb5
Chas Huggins <hugginsc10@gmail.com>
2019-10-08 16:03:57 -0400

    heroku deploy

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -1,7 +1,5 @@
 module.exports = {
-  MONGO_URI: "mongodb+srv://dev:mWqhTAzRxp1xz0jq@stray-jirtj.mongodb.net/dev",
-  secretOrKey: "saveastray",
-  paypal: "ARI7qArxjY0c4IFAyf839AlxOctkRWrEhX5X4KVUaYGwTV6hYiG7nMbTs696vGa1mpCKIu93dAFCcASd",
-  fbookKey: "af0fd18b3f4c012557b57108d1455d8e",
-  fbookClient: "515957642529597"
-};
+  mongoURI: process.env.MONGO_URI,
+  secretOrKey: process.env.SECRET_OR_KEY,
+  
+}
