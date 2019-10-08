1b31e4026573ec21df8ebbf9a3867bd4d4561e15
Chas Huggins <hugginsc10@gmail.com>
2019-10-08 16:44:51 -0400

    deploy to heroku fixed mongo_uri

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -1,6 +1,6 @@
 module.exports = {
-  mongoURI: process.env.MONGO_URI,
-  secretOrKey: process.env.secretOrKey,
+  MONGO_URI: process.env.MONGO_URI,
+  secretOrKey: process.env.SECRET_OR_KEY,
   fbookKey: process.env.fbookKey,
   fbookClient: process.env.fbookClient,
 }
