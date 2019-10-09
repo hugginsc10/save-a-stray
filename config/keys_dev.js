1ddcb0a36200810f01650970198126f5015e5ebc
Chas Huggins <hugginsc10@gmail.com>
2019-10-09 12:34:27 -0400

    add google keys for google oauth2

--- config/keys_dev.js
+++ config/keys_dev.js
@@ -2,5 +2,7 @@ module.exports = {
   MONGO_URI: "mongodb+srv://dev:mWqhTAzRxp1xz0jq@stray-jirtj.mongodb.net/dev",
   secretOrKey: "saveastray",
   fbookKey: "af0fd18b3f4c012557b57108d1455d8e",
-  fbookClient: "515957642529597"
+  fbookClient: "515957642529597",
+  googleClient: "635738163475-uoqtpq3sv1or3frlr5p4ou5b09hbkq3j.apps.googleusercontent.com",
+  googleSecret: "zLG9tWSLUazV7KhYmJJnr32u"
 };
