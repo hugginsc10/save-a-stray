a350946bc503af81b4945212ec16c2c0436e4080
Daniel Hernandez <danielmark252525@gmail.com>
2019-10-08 20:26:35 -0400

    Adds the config file

---
+++ config/keys_dev.js
@@ -0,0 +1,6 @@
+module.exports = {
+  MONGO_URI: "mongodb+srv://dev:mWqhTAzRxp1xz0jq@stray-jirtj.mongodb.net/dev",
+  secretOrKey: "saveastray",
+  fbookKey: "af0fd18b3f4c012557b57108d1455d8e",
+  fbookClient: "515957642529597"
+};
