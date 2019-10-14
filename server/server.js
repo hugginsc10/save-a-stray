df6b4a5a3816c5ffdea746910aa6181c89daaa20
Chas Huggins <hugginsc10@gmail.com>
2019-10-13 21:40:47 -0400

    oauth

--- server/server.js
+++ server/server.js
@@ -179,6 +179,37 @@ passport.deserializeUser(function (obj, cb) {
 
 const app = express();
 
+
+// rfq
+
+// app.use(function (req, res, next) {
+//   /*var err = new Error('Not Found');
+//    err.status = 404;
+//    next(err);*/
+
+//   // Website you wish to allow to connect
+//   res.setHeader('Access-Control-Allow-Origin', '*');
+
+//   // Request methods you wish to allow
+//   res.setHeader('Access-Control-Allow-Methods', 'GET');
+
+//   // Request headers you wish to allow
+//   res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers,X-Access-Token,XKey,Authorization');
+
+//   //  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+
+//   // Pass to next layer of middleware
+//   next();
+// });
+
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
+// rfq
+
+
 app.use(cors());
 app.use(passport.initialize());
 if (!db) {
