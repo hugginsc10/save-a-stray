6ff21b5ae26ed115d950ecd2d709ba4dc6d31032
Tom <tomdriscoll5@gmail.com>
2019-11-07 11:20:06 -0500

    tweaked feed / added App to animalSHow

--- client/src/components/AnimalShow.js
+++ client/src/components/AnimalShow.js
@@ -3,10 +3,11 @@ import {Query,ApolloConsumer} from "react-apollo";
 import Querys from "../graphql/queries";
 import UserShow from "./UserLanding";
 import ShelterShow from "./Shelter";
+import NewApplication from "./Application"
 import './css/AnimalShow.css'
 const { FETCH_ANIMAL} = Querys;
 
-class NewAnimal extends React.Component {
+class AnimalShow extends React.Component {
   constructor(props) {
     super(props);
 
@@ -21,13 +22,19 @@ class NewAnimal extends React.Component {
       video: "",
       application: ""
     };
+    this.show = false
+    this.showForm = this.showForm.bind(this)
   }
 
+  showForm(){
+    document.getElementById('show-application-wrapper').classList.toggle('hidden', false)
+
+  }
   
   
   
   render() {
-     
+     let form = <NewApplication id='show-application' className='hidden'/> 
         return(
             <Query
             query={FETCH_ANIMAL}
@@ -42,19 +49,29 @@ class NewAnimal extends React.Component {
                       return (
                         <div id='animal-show-top'>
                             <h1 id='show-dog-name'>{data.animal.name}</h1>
-                            <img id='show-image' src={data.animal.image}/>
-                            <p>Hi my name is <span>{data.animal.name}</span></p>
-                            <p>I am a {data.animal.age} year old {data.animal.sex}</p>
-                            
-                            <p>My coat is {data.animal.color}</p>
-                            <p>People say: {data.animal.description}</p>
-                            <p>{data.animal.application}</p>
-                            <button id='adopt-button'>Apply to adopt {data.animal.name}</button>
+                            <div id='animal-show-wrapper'>
+                              <img id='show-image' src={data.animal.image}/>
+                              <div id='animal-show-description'>
+
+                                <p>Hi my name is <span>{data.animal.name}</span>.
+                                I am a {data.animal.age} year old {data.animal.sex}.
+                                My coat is {data.animal.color}.
+                                <br/>
+                                People say: {data.animal.description}
+                                {data.animal.application}</p>
+                                <button id='adopt-button' onClick={this.showForm}>Apply to adopt {data.animal.name}</button>
+                              </div>
+                            </div>
+                            <div id='show-application-wrapper' className='hidden'>
+                                {form}
+                            </div>
+                           
                         </div>
                     );
                     }
             }}
             </Query>
+            
         )
     
     
@@ -62,4 +79,4 @@ class NewAnimal extends React.Component {
 
   }
 }
-export default NewAnimal;
+export default AnimalShow;
